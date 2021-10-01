796d0b9f0b2115101dde5e0d3d219f9e568eb6b4
io2tree-avatye <io2tree@avatye.com>
2021-10-01 11:12:56 +0900

    인증정보 입력 가능하도록 수정

--- example-cashroulette-publisher-java/src/main/java/com/avatye/example/publisher/cr/MainActivity.java
+++ example-cashroulette-publisher-java/src/main/java/com/avatye/example/publisher/cr/MainActivity.java
@@ -1,50 +1,84 @@
 package com.avatye.example.publisher.cr;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.avatye.cashroulette.CashRoulettePublisher;
 import com.avatye.example.publisher.cr.databinding.ActivityMainBinding;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final ActivityMainBinding vb = ActivityMainBinding.inflate(LayoutInflater.from(this));
+    private ActivityMainBinding vb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.vb = ActivityMainBinding.inflate(LayoutInflater.from(this));
         setContentView(vb.getRoot());
-        // parameter value
-        final String appUserID = "publisher-example@avatye.com";
-        final String publisherID = App.publisherID;
-        final String publisherAppKey = App.publisherAppKey;
-        final String aaid = "00000000-0000-0000-0000-009876543210";
-        // set appUserID
-        CashRoulettePublisher.setAppUserID(appUserID);
+        setAuthViewBind();
         // click event
+        vb.authButton.setOnClickListener(v -> {
+            final String authAppUserId = Objects.requireNonNull(vb.authAppUserId.getText()).toString();
+            if (authAppUserId.isEmpty()) {
+                Toast.makeText(this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+            } else {
+                CashRoulettePublisher.setAppUserID(authAppUserId);
+                setAuthViewBind();
+            }
+        });
         vb.launchButton.setOnClickListener(v -> {
-            CashRoulettePublisher.launch(this, App.publisherID, App.publisherAppKey, aaid, new CashRoulettePublisher.ILaunchCallback() {
-                @Override
-                public void onSuccess() {
-                    vb.launchResult.setText("onSuccess");
-                    vb.launchBanner.setVisibility(View.VISIBLE);
-                    vb.launchButton.setVisibility(View.INVISIBLE);
-                }
+            CashRoulettePublisher.launch(
+                    this,
+                    App.publisherID,
+                    App.publisherAppKey,
+                    "00000000-0000-0000-0000-009876543210",
+                    new CashRoulettePublisher.ILaunchCallback() {
+                        @Override
+                        public void onSuccess() {
+                            vb.launchResult.setText("Launch: Success");
+                            vb.launchBanner.setVisibility(View.VISIBLE);
+                            vb.launchBannerText.setVisibility(View.GONE);
+                            vb.launchResultText.setVisibility(View.VISIBLE);
+                            vb.launchButton.setEnabled(false);
+                        }
 
-                @Override
-                public void onFailure() {
-                    vb.launchResult.setText("onFailure");
-                    vb.launchBanner.setVisibility(View.GONE);
-                    vb.launchButton.setVisibility(View.VISIBLE);
-                }
-            });
+                        @Override
+                        public void onFailure() {
+                            vb.launchResult.setText("Launch: Failure");
+                            vb.launchBanner.setVisibility(View.GONE);
+                            vb.launchBannerText.setVisibility(View.VISIBLE);
+                            vb.launchResultText.setVisibility(View.GONE);
+                            vb.launchButton.setEnabled(true);
+                        }
+                    });
         });
 
         vb.launchBanner.setOnClickListener(v -> {
-            CashRoulettePublisher.start(this);
+            final String appUserID = CashRoulettePublisher.getAppUserID();
+            if (TextUtils.isEmpty(appUserID)) {
+                Toast.makeText(this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show();
+            } else {
+                CashRoulettePublisher.start(this);
+            }
         });
+    }
+
+    private void setAuthViewBind() {
+        final String appUserID = CashRoulettePublisher.getAppUserID();
+        if (appUserID.isEmpty()) {
+            vb.wrapAuth.setVisibility(View.VISIBLE);
+            vb.wrapLaunch.setVisibility(View.GONE);
+        } else {
+            vb.wrapAuth.setVisibility(View.GONE);
+            vb.wrapLaunch.setVisibility(View.VISIBLE);
+            vb.launchAppUserId.setText(appUserID);
+        }
     }
 }
--- example-cashroulette-publisher-kotlin/src/main/java/com/avatye/example/publisher/cr/MainActivity.kt
+++ example-cashroulette-publisher-kotlin/src/main/java/com/avatye/example/publisher/cr/MainActivity.kt
@@ -2,8 +2,8 @@ package com.avatye.example.publisher.cr
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import com.avatye.cashroulette.CashRoulettePublisher
 import com.avatye.example.publisher.cr.databinding.ActivityMainBinding
@@ -17,37 +17,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(vb.root)
-        // parameter value
-        val appUserID = "publisher-example@avatye.com"
-        val publisherID = App.publisherID
-        val publisherAppKey = App.publisherAppKey
-        val aaid = "00000000-0000-0000-0000-009876543210"
-        // set appUserID
-        CashRoulettePublisher.setAppUserID(appUserID)
+        setAuthViewBind()
         // click event
+        vb.authButton.setOnClickListener {
+            if (vb.authAppUserId.text.isNullOrEmpty()) {
+                Toast.makeText(this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show()
+            } else {
+                val appUserID = vb.authAppUserId.text.toString().trim()
+                CashRoulettePublisher.setAppUserID(appUserID)
+                setAuthViewBind()
+            }
+        }
         vb.launchButton.setOnClickListener {
             CashRoulettePublisher.launch(
                 context = this,
                 publisherID = App.publisherID,
                 publisherAppKey = App.publisherAppKey,
-                aaid = aaid,
+                aaid = "00000000-0000-0000-0000-009876543210",
                 listener = object : CashRoulettePublisher.ILaunchCallback {
                     override fun onSuccess() {
-                        vb.launchResult.text = "onSuccess"
+                        vb.launchResult.text = "Launch: Success"
                         vb.launchBanner.isVisible = true
-                        vb.launchButton.isInvisible = true
+                        vb.launchBannerText.isVisible = false
+                        vb.launchResultText.isVisible = true
+                        vb.launchButton.isEnabled = false
                     }
 
                     override fun onFailure() {
-                        vb.launchResult.text = "onFailure"
+                        vb.launchResult.text = "Launch: Failure"
                         vb.launchBanner.isVisible = false
-                        vb.launchButton.isInvisible = false
+                        vb.launchBannerText.isVisible = true
+                        vb.launchResultText.isVisible = false
+                        vb.launchButton.isEnabled = true
                     }
                 })
         }
-        // entry banner
+
         vb.launchBanner.setOnClickListener {
-            CashRoulettePublisher.start(this)
+            val appUserID = CashRoulettePublisher.getAppUserID()
+            if (appUserID.isNullOrEmpty()) {
+                Toast.makeText(this, "아이디를 입력하세요", Toast.LENGTH_SHORT).show()
+            } else {
+                CashRoulettePublisher.start(this)
+            }
+        }
+    }
+
+    private fun setAuthViewBind() {
+        val appUserID = CashRoulettePublisher.getAppUserID()
+        if (appUserID.isEmpty()) {
+            vb.wrapAuth.isVisible = true
+            vb.wrapLaunch.isVisible = false
+        } else {
+            vb.wrapAuth.isVisible = false
+            vb.wrapLaunch.isVisible = true
+            vb.launchAppUserId.text = appUserID
         }
     }
 
